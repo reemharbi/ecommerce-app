06642a8079c72e2675b65aa1d588e3ba9b98f59c
reemharbi <reemharbi.3@gmail.com>
2020-01-30 11:21:11 +0300

    Create Header and add more styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Homepage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
         <div>
-          {/* Switch matches slash first then it will not render
+            {/* Switch matches slash first then it will not render
           anything else after it. Benefit: gives you more control */}
-          {/* Exact will find the exact match of the url 
+            {/* Exact will find the exact match of the url 
           and render it */}
+            <Header />
             <Switch>
-              <Route exact path='/' component={Homepage} />
-              <Route path='/shop' component={ShopPage} />
+                <Route exact path='/' component={Homepage} />
+                <Route path='/shop' component={ShopPage} />
             </Switch>
         </div>
     );
